b7834ef9e9d787d351fe37bdf29b52da13c5b15e
jahnavi-2010 <80617464+jahnavi-2010@users.noreply.github.com>
2021-06-24 18:51:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+p1="";
+
+Webcam.set({
+width:350,
+height:300,
+image_format:'png',
+png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+
+}
+console.log("ml5 version",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelLoaded);
+function modelLoaded()
+{
+    console.log("model is loaded");
+}
+function check()
+{
+img=document.getElementById("captured_image");
+classifier.classify(img,gotresult);
+}
+function gotresult(error,results)
+{
+if (error)
+{
+    console.error(error);
+}
+else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+
+p1=results[0].label;
+
+
+if (results[0].label=="amazing")
+{
+    document.getElementById("update_emoji").innerHTML="&#128076;";
+}
+if (results[0].label=="best")
+{
+    document.getElementById("update_emoji").innerHTML="&#128077;";
+}
+if (results[0].label=="victory")
+{
+    document.getElementById("update_emoji").innerHTML="&#9996;";
+}
+
+}
+}
+
